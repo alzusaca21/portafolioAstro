808227c996567559db5f50bfc1386eb6f1657bed
Santiao Ordoñez <sa.an.or.pu@gmail.com>
2025-03-10 20:17:01 -0500

    header e inicio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Geist': ['"AR One Sans"', 'sans-serif'],
+      },
+      colors: {
+        'color-text1': '#18181B',
+        'color-text2': '#71717A',
+      },
+    },
   },
   plugins: [],
 }
