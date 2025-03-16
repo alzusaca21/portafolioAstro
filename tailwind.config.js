7420f78f55d675ff9fc4f5f31d97df2f6fdf8f06
Santiao Ordoñez <sa.an.or.pu@gmail.com>
2025-03-15 19:10:24 -0500

    aboutme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,22 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       backgroundImage: {
         "bg-About": "url('images/portfolio1.png')"
       },
       fontFamily: {
-        'Geist': ['"AR One Sans"', 'sans-serif'],
+        'Geist': ['"Big Shoulders"', 'sans-serif'],
       },
       colors: {
-        'color1': '#F9FAFB',
+        'color1': '#e7e7e9',
         'color2': '#71717A',
         'color-blue': '#0A1D7D'
       },
