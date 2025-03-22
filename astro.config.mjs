bb36f68ac14296ac114667c4997f24a48c6a1971
Santiao Ordoñez <sa.an.or.pu@gmail.com>
2025-03-21 23:11:20 -0500

    commit final

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solid from "@astrojs/solid-js";
 import dotenv from "dotenv";
+import netlify from '@astrojs/netlify';
 
 dotenv.config();
 
@@ -10,11 +11,6 @@ export default defineConfig({
         tailwind(),
         solid(),
     ],
-    output: "server", // 🚀 Si usas SSR, asegúrate de definirlo
-    adapter: {
-        name: '@astrojs/node',
-        options: {
-            mode: 'standalone' // 🔥 Especifica el modo requerido
-        }
-    }
+    output: "server",
+    adapter: netlify() 
 });
